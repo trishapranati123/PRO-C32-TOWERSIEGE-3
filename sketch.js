b3e9732dbcf4624581df69cf422af994e1776772
trishapranati123 <70008287+trishapranati123@users.noreply.github.com>
2020-09-25 13:55:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -157,7 +157,7 @@ function keyPressed(){
 }
 
 async function getbackgroundImg(){
-var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+var response = await fetch ("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
 var responseJSON = await response.json ();
 console.log(responseJSON);
 
